13ce36e04ce48c4a6fd2ed259cc3e5e846d1f5a7
benkuly <kontakt@folivo.net>
2024-10-10 13:36:59 +0200

    use fastlane image and android arm

--- fastlane/start_screenshot_emulators.sh
+++ fastlane/start_screenshot_emulators.sh
@@ -2,9 +2,9 @@
 
 ANDROID_PLATFORM=${ANDROID_PLATFORM:-35}
 
-avdmanager create avd -n Nexus_5 -k "system-images;android-${ANDROID_PLATFORM};google_apis;x86_64" -d "Nexus 5"
-avdmanager create avd -n Nexus_7 -k "system-images;android-${ANDROID_PLATFORM};google_apis;x86_64" -d "Nexus 7"
-avdmanager create avd -n Nexus_10 -k "system-images;android-${ANDROID_PLATFORM};google_apis;x86_64" -d "Nexus 10"
+avdmanager create avd -n Nexus_5 -k "system-images;android-${ANDROID_PLATFORM};google_apis;arm64-v8a" -d "Nexus 5"
+avdmanager create avd -n Nexus_7 -k "system-images;android-${ANDROID_PLATFORM};google_apis;arm64-v8a" -d "Nexus 7"
+avdmanager create avd -n Nexus_10 -k "system-images;android-${ANDROID_PLATFORM};google_apis;arm64-v8a" -d "Nexus 10"
 
 mkdir "emulator_logs"
 emulator -avd Nexus_5 -port 5554 -no-window >> emulator_logs/nexus_5.log 2>&1 &
