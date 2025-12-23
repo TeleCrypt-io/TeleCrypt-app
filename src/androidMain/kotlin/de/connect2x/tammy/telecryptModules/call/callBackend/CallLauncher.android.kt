9c427e3416f978ecf5c8ab6fe7306a477f6a10de
Sanya239 <alex.bogdanov05@mail.ru>
2025-12-24 02:41:56 +0300

    CustomTabs for android

--- src/androidMain/kotlin/de/connect2x/tammy/telecryptModules/call/callBackend/CallLauncher.android.kt
+++ src/androidMain/kotlin/de/connect2x/tammy/telecryptModules/call/callBackend/CallLauncher.android.kt
@@ -1,10 +1,15 @@
 package de.connect2x.tammy.telecryptModules.call.callBackend
 
-import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import androidx.browser.customtabs.CustomTabColorSchemeParams
 import androidx.browser.customtabs.CustomTabsIntent
+import androidx.core.graphics.toColorInt
 import androidx.core.net.toUri
+import io.github.oshai.kotlinlogging.KotlinLogging
+
+private val log = KotlinLogging.logger { }
 
 
 /**
@@ -20,19 +25,34 @@ class ElementCallLauncherImpl(private val appContext: Context) : CallLauncher {
     }
 
     override fun joinByUrl(url: String) {
-        val context = appContext
         val uri = url.toUri()
-        val customTabsIntent = CustomTabsIntent.Builder()
-            .setShowTitle(true)
+
+        val elementGreen = "#0DBD8B".toColorInt()
+
+        val colorParams = CustomTabColorSchemeParams.Builder()
+            .setToolbarColor(elementGreen)
             .build()
 
-        if (context is Activity) {
-            customTabsIntent.launchUrl(context, uri)
-        } else {
-            customTabsIntent.intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            customTabsIntent.launchUrl(context, uri)
+        val customTabsIntent = CustomTabsIntent.Builder()
+            .setShowTitle(false)
+            .setUrlBarHidingEnabled(true)
+            .setInstantAppsEnabled(false)
+            .setDefaultColorSchemeParams(colorParams)
+            .build()
+
+        customTabsIntent.intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+        customTabsIntent.intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+
+        try {
+            appContext.packageManager.getPackageInfo("com.android.chrome", 0)
+            customTabsIntent.intent.setPackage("com.android.chrome")
         }
+        catch  (_: PackageManager.NameNotFoundException){
+            log.warn { "no Chrome" } // TODO try access other CustomTabs browsers
+        }
+        customTabsIntent.launchUrl(appContext, uri)
     }
+
 
     override fun isCallAvailable(roomId: String): Boolean {
         // Calls are always available on Android (browser is always present)
