a3caea9dabfc87cfd2ada5898efe3b9394c5d99e
Kuznetsov Artem <st085972@student.spbu.ru>
2025-12-26 11:02:52 +0300

    Use AWT webview window on desktop

--- src/desktopMain/kotlin/de/connect2x/tammy/telecryptModules/call/callBackend/CallLauncher.desktop.kt
+++ src/desktopMain/kotlin/de/connect2x/tammy/telecryptModules/call/callBackend/CallLauncher.desktop.kt
@@ -1,18 +1,43 @@
 package de.connect2x.tammy.telecryptModules.call.callBackend
 
 import com.github.winterreisender.webviewko.WebviewKo
+import com.github.winterreisender.webviewko.WebviewKoAWT
+import java.awt.EventQueue
+import java.awt.Toolkit
+import java.util.function.Consumer
+import javax.swing.JFrame
+import javax.swing.WindowConstants
 
 fun openWebView(url: String) {
-    val webview = WebviewKo()
-    webview.title("telecrypt-messenger call")
-    webview.size(800, 600, WebviewKo.WindowHint.Max)
-    webview.init(
-        "window.addEventListener('load', () => {" +
-            "try { window.focus(); } catch (e) {}" +
-        "});"
-    )
-    webview.show()
-    webview.navigate(url)
+    EventQueue.invokeLater {
+        val frame = JFrame("telecrypt-messenger call")
+        frame.defaultCloseOperation = WindowConstants.DISPOSE_ON_CLOSE
+        val screen = Toolkit.getDefaultToolkit().screenSize
+        frame.setSize((screen.width * 0.75).toInt(), (screen.height * 0.75).toInt())
+        frame.setLocationRelativeTo(null)
+        frame.isAlwaysOnTop = true
+        frame.isAlwaysOnTop = false
+        frame.toFront()
+        frame.requestFocus()
+
+        val view = WebviewKoAWT(
+            0,
+            null,
+            Consumer<WebviewKo> { webview ->
+                webview.title("telecrypt-messenger call")
+                webview.init(
+                    "window.addEventListener('load', () => {" +
+                        "try { window.focus(); } catch (e) {}" +
+                    "});"
+                )
+                webview.navigate(url)
+            }
+        )
+        frame.add(view)
+        frame.isVisible = true
+        frame.toFront()
+        frame.requestFocus()
+    }
 }
 
 fun openUrlInBrowser(url: String) {
